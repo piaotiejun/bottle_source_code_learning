47076da713e6ab1079ba2ab2e33d0a8e8e34ad4b
piaotiejun <674216231@qq.com>
2016-01-12 10:30:21 +0800

    modify

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "init virtural env"
-virtualenv venc
-. venc/bin/activate --no-site-packages
-source ./venc/bin/activate
+virtualenv venc --no-site-packages
+#source venc/bin/activate
+source venc/bin/activate
 pip install -r requirements
+mkdir app
+cp venc/lib/python2.7/site-packages/bottle.py app/
